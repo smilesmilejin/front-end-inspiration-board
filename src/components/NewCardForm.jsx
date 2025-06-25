03d78ae5bd3716f27d594606658d6c6fe4a674cf
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-25 15:05:53 -0400

    deleted comments in NewCardForm.jsx

--- src/components/NewCardForm.jsx
+++ src/components/NewCardForm.jsx
@@ -4,9 +4,6 @@ const kDefaultFormState = {
     message:'',
 };
 
-// const kErrorState = {
-//     message:true,
-// };
 
 const kErrorState = {
     message:'Message cannot be empty',
@@ -28,8 +25,6 @@ const NewCardForm = ({onPostCard}) => {
         console.log("Posting:", { message: trimmedMessage});
         onPostCard({ message: trimmedMessage});
 
-        // onPostBoard(formData);
-
         setFormData(kDefaultFormState);
 
         setErrors(kErrorState);
@@ -45,23 +40,7 @@ const NewCardForm = ({onPostCard}) => {
             return {...formData, [inputName]: inputValue};
         });
 
-        // Clear error if value becomes valid (non-empty after trimming)
-        // if 1 <= 1 length <= 40, error = false
         const trimmedLength = inputValue.trim().length;
-
-        // if (trimmedLength >= 1 && trimmedLength <= 40) {
-        //     setErrors((prev) => ({
-        //         ...prev,
-        //         [inputName]: false,  // no error
-        //     }));
-        // // } else if (trimmedLength < 1 || trimmedLength > 40) {
-        // } else {
-        //     setErrors((prev) => ({
-        //         ...prev,
-        //         [inputName]: true,  // error present
-        //     }));
-        // };
-
 
         if (trimmedLength === 0) {
             setErrors((prev) => ({
@@ -92,13 +71,6 @@ const NewCardForm = ({onPostCard}) => {
                     value={formData[inputName]}
                     placeholder={inputName}
                 />
-                {/* {errors[inputName] && (
-                    <p className="error-text">
-                        {`${inputName} can not be empty or more than 40 characters `}
-                        // {`${inputName.charAt(0).toUpperCase() + inputName.slice(1)} is required`}
-                    </p>
-                )} */}
-
                 {errors[inputName] && (
                     <p>{errors[inputName]}</p>
                 )}
@@ -110,14 +82,9 @@ const NewCardForm = ({onPostCard}) => {
     return (
     <form onSubmit={handleSubmit}>
       <div>
-        {/* <label htmlFor='input-title'>Task Title: </label> */}
         {makeControlledInput('message')}
       </div>
       <div className="button-wrapper">
-        {/* <button disabled={!formData.message.trim() || errors.message}>Create</button> */}
-        {/* <button disabled={errors.message}>Create</button> */}
-        {/* <button disabled={!formData.message.trim()}>Create</button> */}
-
         <button disabled={errors.message}>Create</button>
       </div>
     </form>
