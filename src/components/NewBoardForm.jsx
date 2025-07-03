5ed7e55d0d6a4f65876dc8c7380f4ff806145f91
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-03 14:54:35 -0400

    refactor: code formatting and indentation in NewBoardForm.jsx

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -1,24 +1,25 @@
 import {useState} from 'react';
 import PropTypes from 'prop-types';
-import "../styles/NewBoardForm.css"
+import "../styles/NewBoardForm.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import {faMinus as minusSign} from '@fortawesome/free-solid-svg-icons';
 import { faMinus as minusSign, faPlus as plusSign } from '@fortawesome/free-solid-svg-icons';
 
 const kDefaultFormState = {
-    title:'',
-    owner:'',
+    title: '',
+    owner: '',
 };
 
 const kErrorState = {
-    title:true,
-    owner:true, 
+    title: true,
+    owner: true, 
 };
 
 
 const NewBoardForm = ({onPostBoard}) => {
     const[formData, setFormData] = useState(kDefaultFormState);
     const [errors, setErrors] = useState(kErrorState);
+
     const [formVisible, setFormVisible] = useState(true);
 
     const handleSubmit = (event) => {
@@ -81,36 +82,36 @@ const NewBoardForm = ({onPostBoard}) => {
     };
 
     return (
-    <form className='board-form' onSubmit={handleSubmit}>
-        <div className='form-header'>New Board
-        {/* <div><FontAwesomeIcon icon={minusSign} /></div> */}
-        <FontAwesomeIcon icon={formVisible ? minusSign : plusSign} onClick={() => setFormVisible(!formVisible)}/>
-        </div>
-        {formVisible && (
-          <>
-      <div className='form-field'>
-        {makeControlledInput('title')}
-      </div>
-      <div className = 'form-field' >
-        {makeControlledInput('owner')}
-      </div>
-      {(errors.title || errors.owner) && (
-    <div className="form-errors">
-      {errors.title && (
-        <p className="error-text">Title cannot be empty</p>
-      )}
-      {errors.owner && (
-        <p className="error-text">Owner cannot be empty</p>
-      )}
-    </div>
-  )}
-      <div className="button-wrapper">
-        <button disabled={errors.title || errors.owner}>CREATE</button>
-      </div>
-      </>
-    )}
-    </form>
-  );
+      <form className='board-form' onSubmit={handleSubmit}>
+          <div className='form-header'>
+            New Board
+            {/* <div><FontAwesomeIcon icon={minusSign} /></div> */}
+            <FontAwesomeIcon icon={formVisible ? minusSign : plusSign} onClick={() => setFormVisible(!formVisible)}/>
+          </div>
+
+          {formVisible && (
+            <>
+              <div className='form-field'>
+                {makeControlledInput('title')}
+              </div>
+              <div className='form-field'>
+                {makeControlledInput('owner')}
+              </div>
+
+              {(errors.title || errors.owner) && (
+                <div className="form-errors">
+                  {errors.title && (<p className="error-text">Title cannot be empty</p>)}
+                  {errors.owner && (<p className="error-text">Owner cannot be empty</p>)}
+                </div>
+              )}
+
+              <div className="button-wrapper">
+                <button disabled={errors.title || errors.owner}>CREATE</button>
+              </div>
+            </>
+          )}
+      </form>
+    );
 }
 
 NewBoardForm.propTypes = {
